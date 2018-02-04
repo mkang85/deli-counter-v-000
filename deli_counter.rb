275471a90dda38270511fce57d5cc0483d534a4a
Michael Kang <michaelkang85@gmail.com>
2018-02-04 18:24:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,11 @@ def line(arr)
   if arr.length == 0
     puts "The line is currently empty."
   else
+    string = "The line is currently: "
     arr.each_with_index do|name, index|
-      puts "The line is currently: #{index + 1}. #{name}"
+      string << "#{index + 1}. #{name}"
   end
+  return string
 end
 end
 
